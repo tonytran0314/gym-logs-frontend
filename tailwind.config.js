a87de37ce82c925ee05e7a6506ebfa1a3beefc47
Gia Huy Tran <tonytran0314@gmail.com>
2024-10-04 10:52:11 -0400

    tailwindcss v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,13 @@
 export default {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
-  variants: {
-    extend: {},
-  },
+  plugins: []
 }
 
