179e2f93ff59129be4f32628fa1a9e490991c1a3
Gia Huy Tran <tonytran0314@gmail.com>
2024-10-21 20:59:03 -0400

    feat: spinner button component

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,22 +1,27 @@
 <script setup>
     import { useAuthStore } from '@/stores/authStore'
+    import SpinnerButton from '@/components/SpinnerButton.vue'
 
     const auth = useAuthStore()
 </script>
 
 <template>
-    <div>
+    <!-- <div>
         <router-link to="/login" class="text-3xl">Login</router-link>
         <router-link to="/signup" class="text-3xl">Signup</router-link>
+    </div> -->
+    
+    <div class="p-8">
+        <form @submit.prevent="auth.logout">
+            <SpinnerButton label="Đăng xuất" />
+        </form>
+
+        <br>
+        
+        <form @submit.prevent="auth.fetchInfo">
+            <button type="submit">Fetch Info</button>
+        </form>
     </div>
-    
-    <form @submit.prevent="auth.logout">
-        <button type="submit">Log out</button>
-    </form>
-    
-    <form @submit.prevent="auth.fetchInfo">
-        <button type="submit">Fetch Info</button>
-    </form>
 </template>
 
 <style>
