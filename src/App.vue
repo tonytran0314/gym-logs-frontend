44cd07f635c08ed7e5ead32b876066ac8c5ba60f
Gia Huy Tran <tonytran0314@gmail.com>
2024-10-27 22:37:50 -0400

    create base modal

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,8 @@
+<script setup>
+  import BaseModal from '@/components/modals/BaseModal.vue'
+</script>
+
 <template>
   <RouterView />
+  <BaseModal />
 </template>
--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,25 +1,33 @@
 <script setup>
     import { useAuthStore } from '@/stores/authStore'
+    import { useModalStore } from '@/stores/modalStore'
+    
     import LoadingButton from '@/components/form/LoadingButton.vue'
     import Button from '@/components/form/Button.vue'
+    import TestModal from '@/components/modals/TestModal.vue'
 
     const auth = useAuthStore()
+    const modal = useModalStore()
+
+    const startExerciseDay = () => {
+        modal.open(TestModal)
+    }
 </script>
 
 <template>
-    <div>
-        <router-link to="/login" class="text-3xl">Login</router-link>
-        <router-link to="/signup" class="text-3xl">Signup</router-link>
-    </div>
+    <div class="p-8 space-y-8">
+        <div class="space-x-8">
+            <router-link to="/login" class="text-3xl">Login</router-link>
+            <router-link to="/signup" class="text-3xl">Signup</router-link>
+        </div>
     
-    <div class="p-8">
         <form @submit.prevent="auth.logout">
             <LoadingButton v-if="auth.isLoading" />
             <Button v-else>Đăng xuất</Button>
         </form>
 
-        <br>
-        
+        <button @click="startExerciseDay" type="button" class="border py-4 px-8 bg-emerald-600 text-white rounded-lg">Workout now</button>
+
         <form @submit.prevent="auth.fetchInfo">
             <button type="submit">Fetch Info</button>
         </form>
