ba0b9eb49c4a7afd8f4ed942cd5542a5fdd2b123
Gia Huy Tran <tonytran0314@gmail.com>
2024-11-11 15:47:16 -0500

    fix: nav item not showing border

--- src/components/navigation/NavBar.vue
+++ src/components/navigation/NavBar.vue
@@ -34,8 +34,11 @@
                 <li v-for="menu in menus">
                     <router-link 
                         :to="menu.destination"
-                        class="block hover:bg-emerald-100 font-bold hover:text-emerald-600 pl-7 pt-4 pb-4 rounded border-l-4 border-white hover:border-emerald-600"
-                        :class="{'bg-emerald-100 text-emerald-600 border-emerald-600' : isCurrentRoute(menu.destination)}"
+                        class="block font-bold pl-7 pt-4 pb-4 rounded border-l-4 hover:bg-emerald-100 hover:border-emerald-600 hover:text-emerald-600"
+                        :class="
+                            {'bg-emerald-100 text-emerald-600 border-emerald-600' : isCurrentRoute(menu.destination)},
+                            {'border-white' : !isCurrentRoute(menu.destination)}
+                        "
                         >
                             {{ menu.label }}
                     </router-link>
