4b18e380c205fdee05742c3f1e59728d5a192396
Gia Huy Tran <tonytran0314@gmail.com>
2024-11-28 01:48:33 -0500

    style: end set modal

--- src/components/modals/EndSetModal.vue
+++ src/components/modals/EndSetModal.vue
@@ -1,6 +1,7 @@
 <script setup>
   import CloseButton from '@/components/modals/CloseButton.vue'
   import Button from '@/components/form/Button.vue'
+  import Input from '@/components/form/Input.vue'
 
   import { useRouter } from 'vue-router'
   import { useModalStore } from '@/stores/modalStore'
@@ -35,16 +36,29 @@
 </script>
 
 <template>
-    <div class="bg-white space-y-8 p-8 rounded-xl">
-        <form @submit.prevent="saveThisSet">
-            <div>
-                <input v-model="set.weight_level" type="text" placeholder="weight" required>
-            </div>
-            <div>
-                <input v-model="set.reps" type="text" placeholder="reps" required>
-            </div>
-            <Button>Take a short break</Button>
-        </form>
-        <CloseButton />
+  <div class="bg-white dark:bg-gray-800 space-y-4 p-8 rounded-xl">
+
+    <div class="flex justify-between">
+      <p class="text-3xl text-gray-900 dark:text-blue-50">Pounds and reps</p>
+      <CloseButton />
     </div>
+
+    <form @submit.prevent="saveThisSet" class="space-y-6">
+      <div class="flex gap-4">
+        <Input
+          v-model="set.weight_level"
+          label="Weight level (lbs)"
+          type="text"
+          name="weightLevel"
+          placeholder="25" />
+        <Input
+            v-model="set.reps"
+            label="Reps"
+            type="text"
+            name="reps"
+            placeholder="18" />
+      </div>
+      <Button class="w-full">Take a short break</Button>
+    </form>
+  </div>
 </template>
