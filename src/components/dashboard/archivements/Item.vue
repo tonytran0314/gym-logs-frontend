99b57a865ed13ea870f5e5cb79dba3c9aa2d882b
Gia Huy Tran <tonytran0314@gmail.com>
2024-12-07 01:57:43 -0500

    style: light/dark mode for stat cards

--- src/components/dashboard/archivements/Item.vue
+++ src/components/dashboard/archivements/Item.vue
@@ -3,14 +3,14 @@
 </script>
 
 <template>
-    <div class="text-white p-4 rounded-xl w-full hover:shadow-lg flex flex-col gap-4"
-        :class=" props.highlighted ? 'bg-emerald-600' : 'bg-gray-800' ">
+    <div class="text-gray-900 dark:text-blue-50 p-4 rounded-xl w-full shadow-lg flex flex-col gap-4"
+        :class=" props.highlighted ? 'text-white bg-emerald-600' : 'bg-white dark:bg-gray-800' ">
         <div class="flex justify-between">
             <div class="text-xl line-clamp-2 min-w-44">
                 <slot name="title"></slot>
             </div>
             <div class="min-w-16 min-h-16 max-w-16 max-h-16 rounded-2xl flex justify-center items-center"
-                :class="props.highlighted ? 'text-emerald-600 bg-emerald-100' : 'text-emerald-300 bg-gray-700' ">
+                :class="props.highlighted ? 'text-emerald-600 bg-emerald-100' : 'bg-emerald-100 text-emerald-600 dark:text-emerald-300 dark:bg-gray-700' ">
                 <slot name="icon"></slot>
             </div>
         </div>
