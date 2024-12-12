32eb3c76b8918f47f26d3ec1208fabb210f7ad04
Gia Huy Tran <tonytran0314@gmail.com>
2024-12-12 02:44:41 -0500

    responsive for on set screen

--- src/components/workout/CurrentExercise.vue
+++ src/components/workout/CurrentExercise.vue
@@ -21,17 +21,17 @@
 </script>
 
 <template>
-    <div class="flex flex-col h-full justify-between">
-        <div class="space-y-10">
-            <p class="text-4xl font-bold">Working out...</p>
+    <div class="flex flex-col h-full justify-between space-y-6">
+        <div class="space-y-6 lg:space-y-10">
+            <p class="text-2xl sm:text-3xl lg:text-4xl font-bold text-center">Working out...</p>
             <div class="flex flex-col gap-6 justify-center items-center">
-                <p class="text-3xl">{{ exercise.currentExercise.name }}</p>
-                <p class="text-5xl font-bold">Set #{{ exercise.currentExercise.set }}</p>
+                <p class="text-xl sm:text-2xl lg:text-3xl">{{ exercise.currentExercise.name }}</p>
+                <p class="text-3xl sm:text-4xl lg:text-5xl font-bold">Set #{{ exercise.currentExercise.set }}</p>
             </div>
         </div>
         
-        <div class="flex gap-4">
-            <div @click="backToExerciseSelection" v-if="exercise.currentExercise.set === 1" class="text-gray-900 dark:text-blue-50 cursor-pointer bg-gray-500 px-6 py-2 rounded-lg hover:bg-gray-600">Back</div>
+        <div class="flex flex-col-reverse md:flex-row gap-4">
+            <div @click="backToExerciseSelection" v-if="exercise.currentExercise.set === 1" class="text-gray-900 dark:text-blue-50 cursor-pointer bg-gray-500 px-6 py-2 rounded-lg hover:bg-gray-600 text-center">Back</div>
             <Button @click="finishSet" class="w-full">I Have Finished This Set</Button>
         </div>
     </div>
