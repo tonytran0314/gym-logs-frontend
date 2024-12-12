c1875006c4cd0025cab077392f956a33afb63d00
Gia Huy Tran <tonytran0314@gmail.com>
2024-12-12 08:33:59 -0500

    fix overflow name on user status

--- src/components/navigation/UserStatus.vue
+++ src/components/navigation/UserStatus.vue
@@ -12,13 +12,12 @@
             <font-awesome-icon :icon="['fas', 'user']" class="size-6 text-emerald-100" />
         </div>
         <div>
-            <p class="font-bold pt-1 dark:text-blue-50">{{ profile.user.fullname }}</p>
+            <p class="font-bold pt-1 dark:text-blue-50 w-[136px] line-clamp-1">{{ profile.user.fullname }}</p>
             <p class="text-sm text-gray-500 dark:text-gray-300 space-x-2 flex items-center">
                 <span>Have a good day!</span>
                 <!-- <span class="inline-block size-3 rounded-full bg-emerald-300"></span> -->
             </p> 
             <!-- Sau chữ Đang tập luyện có dấu chấm xanh như FB, suy nghĩ xem không tập thì để chữ gì. Hoặc cho người ta tự custom như liên minh -->
         </div>
-        <!-- Chỗ này có cái dấu 3 chấm hay mũi tên đi lên hoặc mũi tên chỉ bên phải gì đó, bấm vô nó mở menu nhỏ nhỏ -->
     </div>
 </template>
