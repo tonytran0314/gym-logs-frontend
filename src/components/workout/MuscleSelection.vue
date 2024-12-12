2674c2001c3dd25afe0b8549b393389113e95d0a
Gia Huy Tran <tonytran0314@gmail.com>
2024-12-12 08:23:52 -0500

    images not working on the production

--- src/components/workout/MuscleSelection.vue
+++ src/components/workout/MuscleSelection.vue
@@ -33,7 +33,7 @@
       <div class="flex flex-wrap justify-center gap-6 h-80">
         <div v-for="muscle in exercise.muscles" :key="muscle.id" @click="selectMuscle(muscle.id)" class="w-40 h-[160px] bg-white text-gray-900 dark:text-blue-50 dark:bg-gray-800 rounded-xl py-6 px-10 hover:cursor-pointer hover:bg-emerald-500 hover:text-white dark:hover:bg-emerald-600 flex flex-col items-center justify-center gap-3">
           <div>
-            <img class="w-16" :src="storage + muscle.image" alt="muscle image">
+            <img class="w-16" :src="storage + '/' + muscle.image" alt="muscle image">
           </div>
           <p class="capitalize">{{ muscle.name }}</p>
         </div>
