0abce15d9d17cd59ccea55b16c19efc496f02ee0
Gia Huy Tran <tonytran0314@gmail.com>
2024-12-12 04:30:56 -0500

    responsive for the history skeleton loading

--- src/components/loading_animation/History.vue
+++ src/components/loading_animation/History.vue
@@ -1,50 +1,76 @@
 <template>
-    <div class="space-y-6">
-        <div class="flex justify-between flex-grow bg-white dark:bg-gray-800 p-8 rounded-2xl items-center h-auto w-full">
-            <div class="flex gap-16 animate-pulse">
-                <div class="flex flex-col gap-3 items-center">
-                    <div class="h-9 w-16 rounded-lg bg-gray-200 dark:bg-gray-700"></div>
-                    <div class="h-9 w-20 rounded-lg bg-gray-200 dark:bg-gray-700"></div>
+    <div class="space-y-6">        
+        <div class="flex flex-col lg:flex-row lg:justify-between gap-8 bg-white dark:bg-gray-800 p-8 rounded-2xl text-gray-900 dark:text-blue-50">
+            <div class="flex flex-col lg:flex-row gap-8 w-full animate-pulse">
+                <div class="flex lg:flex-col gap-2 items-center text-2xl text-gray-900 dark:text-blue-50">
+                    <div class="w-32 h-6 bg-gray-200 dark:bg-gray-700 rounded-xl"></div>
+                    <div class="hidden lg:block w-28 h-6 bg-gray-200 dark:bg-gray-700 rounded-xl"></div>
                 </div>
-                <div class="flex flex-col gap-6">
-                    <div class="h-7 w-72 rounded-full bg-gray-200 dark:bg-gray-700"></div>
-                    <div class="h-7 w-80 rounded-full bg-gray-200 dark:bg-gray-700"></div>
-                    <div class="h-7 w-96 rounded-full bg-gray-200 dark:bg-gray-700"></div>
+                <div class="flex flex-col gap-6 max-h-36 line-clamp-3 w-full">
+                    <div class="flex justify-between w-full gap-3">
+                        <div class="w-40 md:w-96 lg:w-72 h-6 bg-gray-200 dark:bg-gray-700 rounded-xl"></div>
+                        <div class="w-16 h-6 bg-gray-200 dark:bg-gray-700 rounded-xl"></div>
+                    </div>
+                    <div class="flex justify-between w-full gap-3">
+                        <div class="w-24 md:w-60 lg:w-52 h-6 bg-gray-200 dark:bg-gray-700 rounded-xl"></div>
+                        <div class="w-16 h-6 bg-gray-200 dark:bg-gray-700 rounded-xl"></div>
+                    </div>
+                    <div class="flex justify-between w-full gap-3">
+                        <div class="w-36 md:w-80 lg:w-60 h-6 bg-gray-200 dark:bg-gray-700 rounded-xl"></div>
+                        <div class="w-16 h-6 bg-gray-200 dark:bg-gray-700 rounded-xl"></div>
+                    </div>
                 </div>
             </div>
-            <div class="w-24 h-10 rounded-lg bg-gray-200 dark:bg-gray-700 animate-pulse"></div>
+            <div class="w-full lg:w-24 bg-gray-200 dark:bg-gray-700 px-4 py-2 rounded-xl h-10 animate-pulse"></div>
         </div>
 
-        
 
         <!-- DUPLICATES -->
-        <div class="flex justify-between flex-grow bg-white dark:bg-gray-800 p-8 rounded-2xl items-center h-auto w-full">
-            <div class="flex gap-16 animate-pulse">
-                <div class="flex flex-col gap-3 items-center">
-                    <div class="h-9 w-16 rounded-lg bg-gray-200 dark:bg-gray-700"></div>
-                    <div class="h-9 w-20 rounded-lg bg-gray-200 dark:bg-gray-700"></div>
+        <div class="flex flex-col lg:flex-row lg:justify-between gap-8 bg-white dark:bg-gray-800 p-8 rounded-2xl text-gray-900 dark:text-blue-50">
+            <div class="flex flex-col lg:flex-row gap-8 w-full animate-pulse">
+                <div class="flex lg:flex-col gap-2 items-center text-2xl text-gray-900 dark:text-blue-50">
+                    <div class="w-32 h-6 bg-gray-200 dark:bg-gray-700 rounded-xl"></div>
+                    <div class="hidden lg:block w-28 h-6 bg-gray-200 dark:bg-gray-700 rounded-xl"></div>
                 </div>
-                <div class="flex flex-col gap-6">
-                    <div class="h-7 w-72 rounded-full bg-gray-200 dark:bg-gray-700"></div>
-                    <div class="h-7 w-80 rounded-full bg-gray-200 dark:bg-gray-700"></div>
-                    <div class="h-7 w-96 rounded-full bg-gray-200 dark:bg-gray-700"></div>
+                <div class="flex flex-col gap-6 max-h-36 line-clamp-3 w-full">
+                    <div class="flex justify-between w-full gap-3">
+                        <div class="w-40 md:w-96 lg:w-72 h-6 bg-gray-200 dark:bg-gray-700 rounded-xl"></div>
+                        <div class="w-16 h-6 bg-gray-200 dark:bg-gray-700 rounded-xl"></div>
+                    </div>
+                    <div class="flex justify-between w-full gap-3">
+                        <div class="w-24 md:w-60 lg:w-52 h-6 bg-gray-200 dark:bg-gray-700 rounded-xl"></div>
+                        <div class="w-16 h-6 bg-gray-200 dark:bg-gray-700 rounded-xl"></div>
+                    </div>
+                    <div class="flex justify-between w-full gap-3">
+                        <div class="w-36 md:w-80 lg:w-60 h-6 bg-gray-200 dark:bg-gray-700 rounded-xl"></div>
+                        <div class="w-16 h-6 bg-gray-200 dark:bg-gray-700 rounded-xl"></div>
+                    </div>
                 </div>
             </div>
-            <div class="w-24 h-10 rounded-lg bg-gray-200 dark:bg-gray-700 animate-pulse"></div>
+            <div class="w-full lg:w-24 bg-gray-200 dark:bg-gray-700 px-4 py-2 rounded-xl h-10 animate-pulse"></div>
         </div>
-        <div class="flex justify-between flex-grow bg-white dark:bg-gray-800 p-8 rounded-2xl items-center h-auto w-full">
-            <div class="flex gap-16 animate-pulse">
-                <div class="flex flex-col gap-3 items-center">
-                    <div class="h-9 w-16 rounded-lg bg-gray-200 dark:bg-gray-700"></div>
-                    <div class="h-9 w-20 rounded-lg bg-gray-200 dark:bg-gray-700"></div>
+        <div class="flex flex-col lg:flex-row lg:justify-between gap-8 bg-white dark:bg-gray-800 p-8 rounded-2xl text-gray-900 dark:text-blue-50">
+            <div class="flex flex-col lg:flex-row gap-8 w-full animate-pulse">
+                <div class="flex lg:flex-col gap-2 items-center text-2xl text-gray-900 dark:text-blue-50">
+                    <div class="w-32 h-6 bg-gray-200 dark:bg-gray-700 rounded-xl"></div>
+                    <div class="hidden lg:block w-28 h-6 bg-gray-200 dark:bg-gray-700 rounded-xl"></div>
                 </div>
-                <div class="flex flex-col gap-6">
-                    <div class="h-7 w-72 rounded-full bg-gray-200 dark:bg-gray-700"></div>
-                    <div class="h-7 w-80 rounded-full bg-gray-200 dark:bg-gray-700"></div>
-                    <div class="h-7 w-96 rounded-full bg-gray-200 dark:bg-gray-700"></div>
+                <div class="flex flex-col gap-6 max-h-36 line-clamp-3 w-full">
+                    <div class="flex justify-between w-full gap-3">
+                        <div class="w-40 md:w-96 lg:w-72 h-6 bg-gray-200 dark:bg-gray-700 rounded-xl"></div>
+                        <div class="w-16 h-6 bg-gray-200 dark:bg-gray-700 rounded-xl"></div>
+                    </div>
+                    <div class="flex justify-between w-full gap-3">
+                        <div class="w-24 md:w-60 lg:w-52 h-6 bg-gray-200 dark:bg-gray-700 rounded-xl"></div>
+                        <div class="w-16 h-6 bg-gray-200 dark:bg-gray-700 rounded-xl"></div>
+                    </div>
+                    <div class="flex justify-between w-full gap-3">
+                        <div class="w-36 md:w-80 lg:w-60 h-6 bg-gray-200 dark:bg-gray-700 rounded-xl"></div>
+                        <div class="w-16 h-6 bg-gray-200 dark:bg-gray-700 rounded-xl"></div>
+                    </div>
                 </div>
             </div>
-            <div class="w-24 h-10 rounded-lg bg-gray-200 dark:bg-gray-700 animate-pulse"></div>
+            <div class="w-full lg:w-24 bg-gray-200 dark:bg-gray-700 px-4 py-2 rounded-xl h-10 animate-pulse"></div>
         </div>
     </div>
 </template>
