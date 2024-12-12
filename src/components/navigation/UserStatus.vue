6ee7b968995ac731790ee33719f90af043685486
Gia Huy Tran <tonytran0314@gmail.com>
2024-12-11 23:26:51 -0500

    style for user status

--- src/components/navigation/UserStatus.vue
+++ src/components/navigation/UserStatus.vue
@@ -8,12 +8,14 @@
 
 <template>
     <div class="flex space-x-2 relative">
-        <div class="size-12 rounded-full bg-gray-500"></div>
+        <div class="size-12 rounded-full bg-emerald-600 flex items-center justify-center">
+            <font-awesome-icon :icon="['fas', 'user']" class="size-6 text-emerald-100" />
+        </div>
         <div>
             <p class="font-bold pt-1 dark:text-blue-50">{{ profile.user.fullname }}</p>
             <p class="text-sm text-gray-500 dark:text-gray-300 space-x-2 flex items-center">
-                <span>Working out</span>
-                <span class="inline-block size-3 rounded-full bg-emerald-300"></span>
+                <span>Have a good day!</span>
+                <!-- <span class="inline-block size-3 rounded-full bg-emerald-300"></span> -->
             </p> 
             <!-- Sau chữ Đang tập luyện có dấu chấm xanh như FB, suy nghĩ xem không tập thì để chữ gì. Hoặc cho người ta tự custom như liên minh -->
         </div>
