893a39d481ae72c86245879e22695c1c3b4ac7c7
Gia Huy Tran <tonytran0314@gmail.com>
2024-12-18 20:40:11 -0500

    responsive for empty history message

--- src/components/history/EmptyMessage.vue
+++ src/components/history/EmptyMessage.vue
@@ -13,21 +13,21 @@
 <template>
     <div class="flex flex-col justify-center items-center gap-8 w-full h-[calc(100vh-152px)] bg-white dark:bg-gray-800 p-8 rounded-2xl text-gray-900 dark:text-blue-50">
         <div class="bg-blue-50 dark:bg-gray-900 rounded-full">
-            <EmptyIllustration class="w-96" />
+            <EmptyIllustration class="w-40 md:w-52 lg:w-96" />
         </div>
         <div class="flex flex-col items-center gap-4">
-            <div class="text-3xl font-bold">
+            <div class="text-xl sm:text-2xl lg:text-3xl font-bold">
                 No History Yet
             </div>
-            <div class="flex flex-col items-center text-gray-500 dark:text-gray-300">
+            <div class="flex flex-col items-center text-gray-500 dark:text-gray-300 text-sm sm:text-md text-center">
                 <p>You currently do not have a history of workout days.</p>
                 <p>You can workout now.</p>
             </div>
         </div>
         
         <Button @click="selectMuscleAndExercise" class="flex gap-2 items-center">
-            <font-awesome-icon :icon="['fas', 'dumbbell']" class="size-6" />
-            <span>Workout now</span>
+            <font-awesome-icon :icon="['fas', 'dumbbell']" class="size-6 hidden sm:block md:size-4 lg:size-6" />
+            <span class="text-xs sm:text-sm md:text-md">Workout now</span>
         </Button>
     </div>
 </template>
