b1cbe395d0ed26a1c729557e7dd59c847637d815
Trần Gia Huy <tonytran0314@gmail.com>
2025-03-29 03:38:00 -0400

    fix: theme colors

--- src/components/workout/OnSet.vue
+++ src/components/workout/OnSet.vue
@@ -6,7 +6,7 @@
 <template>
     <div class="flex gap-8 flex-col lg:flex-row items-center">
         <OnSetIllustration class="w-40 md:w-52 lg:w-96" />
-        <div class="flex flex-col gap-8 bg-gray-800 p-8 rounded-2xl">
+        <div class="flex flex-col gap-8 bg-white shadow-lg dark:bg-gray-800 p-8 rounded-2xl">
             <div class="text-gray-900 dark:text-blue-50 flex flex-col gap-8 h-full">
                 <Suspense>
                     <CurrentExercise />
--- src/components/workout/Rest.vue
+++ src/components/workout/Rest.vue
@@ -46,14 +46,14 @@
 <template>
     <div class="flex gap-8 flex-col lg:flex-row items-center">
         <RestIllustration class="w-40 md:w-52 lg:w-96" />
-        <div class="bg-white dark:bg-gray-800 p-8 rounded-2xl flex flex-col items-center gap-10 justify-between">
+        <div class="bg-white dark:bg-gray-800 p-8 rounded-2xl flex flex-col items-center gap-10 justify-between shadow-lg">
             <p class="text-gray-900 dark:text-blue-50 text-2xl sm:text-3xl lg:text-4xl font-bold">Take a short break ...</p>
             <p class="text-gray-900 dark:text-blue-50 text-7xl sm:text-8xl lg:text-9xl">{{ restCountdown.minutes }}:{{ restCountdown.seconds }}</p>
 
 
             <div class="flex flex-col-reverse md:flex-row gap-4">
-                <div @click="stopExercise" class="text-gray-900 dark:text-blue-50 cursor-pointer bg-gray-500 px-6 py-2 rounded-lg hover:bg-gray-600">Stop the Exercise</div>
-                <div @click="gotoNextSet" class="text-gray-900 dark:text-blue-50 cursor-pointer bg-emerald-600 px-6 py-2 rounded-lg hover:bg-emerald-700 disabled">Next Set</div>
+                <div @click="stopExercise" class="text-white cursor-pointer bg-gray-500 px-6 py-2 rounded-lg hover:bg-gray-600">Stop the Exercise</div>
+                <div @click="gotoNextSet" class="text-white cursor-pointer bg-emerald-600 px-6 py-2 rounded-lg hover:bg-emerald-700 disabled">Next Set</div>
             </div>
         </div>
     </div>
