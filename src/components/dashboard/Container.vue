29c3766cd5530a9198a743771dad181afcb9f8a1
Trần Gia Huy <tonytran0314@gmail.com>
2025-05-14 02:39:46 -0400

    install shadcn-vue

--- src/components/dashboard/Container.vue
+++ src/components/dashboard/Container.vue
@@ -3,12 +3,18 @@
     import PieChart from '@/components/dashboard/charts/PieChart.vue'
     import Achievement from '@/components/dashboard/achievements/Container.vue'
     import DashboardLoadingAnimation from '@/components/loading_animation/Dashboard.vue'
+    
+import { Button } from '@/components/ui/button'
+import { MailOpen } from 'lucide-vue-next'
 </script>
 
 <template>
     <div>
         <Suspense>
             <div class="space-y-6">
+  <Button>
+    <MailOpen class="w-4 h-4 mr-2" /> Login with Email
+  </Button>
                 <Achievement />
                 <div class="flex flex-col xl:flex-row gap-6 w-full h-96 text-center">
                     <div class="w-full xl:w-3/4 h-96 bg-white dark:bg-gray-800 rounded-2xl pt-8 pr-8 pl-8 pb-14"><LineChart /></div>
